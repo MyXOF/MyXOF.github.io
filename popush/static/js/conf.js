03c45fb8d775f5628132deb362eb60ab0bffe36f
Dark Templar <xuyi556677@163.com>
2014-10-11 22:20:28 +0800

    no message

---
+++ popush/static/js/conf.js
@@ -0,0 +1,12 @@
+var app = app || {};
+
+app.Package = {
+  VERSION: '1.0.3',
+  SOCKET_IO: (window.location.href.substring(0,4) != 'file') ? '/' : 'http://localhost:4444/',
+  SAVE_TIME_OUT: 1000,
+  ENABLE_RUN: true,
+  ENABLE_DEBUG: true,
+  ROUTE_ROOT: '/',
+  CODE_MIRROR_MODEURL: 'js/lib/codemirror/%N.js',
+  ALLOW_MISS: true,
+};
